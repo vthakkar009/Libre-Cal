12b33941a7d59423a47baf9ebe85d25af710d074
DESKTOP-5K5PUU2\vthak <vthakkar@luc.edu>
2018-04-22 15:46:57 -0500

    ..

--- src/TheCal.java
+++ src/TheCal.java
@@ -7,5 +7,15 @@ import java.util.*;
 
 public class TheCal {
 	
+	static JLabel LabelMonth, LabelYear;
+	static JButton buttonPrev, buttonNext;
+	static JTable tabelCal;
+	static JComboBox comboYear;
+	static JFrame mainFrame;
+	static Container pane;
+	static JScrollPane theScrollPane;
+	static JPanel panelCal;
+	static int Year, Month, Day, currentYear, currentMonth;
+	
 
 }
